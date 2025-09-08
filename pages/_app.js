7138d2250cb8ad8147fbc7063fecb3eeb5bbb6fc
Cameron2920 <caramsay@uwaterloo.ca>
2025-09-08 15:24:56 -0400

    Updated favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import "@/app/globals.css"; // adjust path if you put it in styles/global.css
+import "@/app/globals.css";
+import Head from "next/head"; // adjust path if you put it in styles/global.css
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" type="image/png" sizes="1024x1024" href="/favicon.png" />
+        <title>Cycle Log</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
